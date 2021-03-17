72c431f99b6c9c5141e4723dd7e1c70280e9b78b
Kausthub Thekke Madathil <kausthubtm6671@gmail.com>
2021-03-17 22:52:36 +0530

    added basic socket programming and cpu scheduling

--- simulator/urls.py
+++ simulator/urls.py
@@ -5,5 +5,9 @@ from django.urls import path
 app_name = 'simulator'
 urlpatterns = [
     path('', views.get_index, name='get_index'),
-    path('cpuAlgo/<int:id>/', views.cpuSchedular, name='cpuSchedular'),
+    path('cpuAlgo/',views.cpuScehdular, name='cpuScehdular'),
+    path('cpuAlgo/<int:id>/', views.cpuAlgo, name='cpuAlgo'),
+    path('sockets/',views.sockets, name='sockets'),
+    path('sockets/tcp', views.tcp, name='tcp'),
+    path('sockets/udp', views.udp, name='udp')
 ]
--- simulator/views.py
+++ simulator/views.py
@@ -6,6 +6,18 @@ from .models import *
 def get_index(request):
     return render(request, 'index.html')
 
-def cpuSchedular(request, id):
+def cpuScehdular(request):
+    return render(request, 'cpuAlgos/cpu_main.html')
+
+def cpuAlgo(request, id):
     algo = CpuAlgo.objects.filter(algo_id=id)[0]
-    return render(request, 'cpuAlgos/cpu.html', {'algo': algo})
+    return render(request, 'cpuAlgos/cpu_algo.html', {'algo': algo})
+
+def sockets(request):
+    return render(request, 'sockets/sockets_main.html')
+
+def tcp(request):
+    return render(request, 'sockets/tcp.html')
+
+def udp(request):
+    return render(request, 'sockets/udp.html')
